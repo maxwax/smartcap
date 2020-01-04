36963f6079aad39decfdd99b0b6b6b45cbea2ed2
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-01-03 18:06:02 -0700

    * Deploy to /usr/local/sbin not /usr/local/bin. Script requires root|sudo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # smartcap Versions
 
+## 1.0.2 Publish
+
+* Deploy to /usr/local/sbin not /usr/local/bin. Script requires root|sudo
+
 ## 1.0.1 Publish
 
 * Rename script smartcap.sh to smartcap for consistency
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Deploy smartcap to /usr/local/bin"
-sudo cp -pr smartcap /usr/local/bin
-sudo chmod a+rx /usr/local/bin/smartcap
+sudo cp -pr smartcap /usr/local/sbin
+sudo chmod a+rx /usr/local/sbin/smartcap
